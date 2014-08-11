953be8f49edf7db79a1599ae7d4447b6ef4338a0
egocarib <egocaribx@gmail.com>
2014-08-11 18:46:57 -0500

    Internal Funcs

---
+++ EA_Internal.cpp
@@ -0,0 +1,212 @@
+
+#include "EA_Internal.h"
+#include "EA_EffectLib.h"
+
+//___________________________________________________________________________________________________________
+//============================== EQUIPMENT BORROWED FROM PAPYRUSWORNOBJECT.CPP ==================================
+//
+//                                       Thank you, SKSE team!
+
+EquippedEnchantMap			eq_map;
+ActorEnchantedWeaponInfo	enchantedWeapon_map;
+
+
+class MatchByEquipSlot : public FormMatcher
+{
+	UInt32 m_mask;
+	UInt32 m_hand;
+	Actor * m_actor;
+public:
+	MatchByEquipSlot(Actor * actor, UInt32 hand, UInt32 slot) : 
+	  m_hand(hand),
+	  m_mask(slot),
+	  m_actor(actor)
+	  {
+
+	  }
+
+	  enum
+	  {
+		  kSlotID_Left = 0,
+		  kSlotID_Right
+	  };
+
+	  bool Matches(TESForm* pForm) const {
+		  if (pForm) {
+			  if(pForm->formType != TESObjectWEAP::kTypeID) { // If not a weapon use mask
+				  BGSBipedObjectForm* pBip = DYNAMIC_CAST(pForm, TESForm, BGSBipedObjectForm);
+				  if (pBip)
+					  return (pBip->data.parts & m_mask) != 0;
+			  } else if(m_mask == 0) { // Use hand if no mask specified
+				  TESForm * equippedForm = m_actor->GetEquippedObject(m_hand == kSlotID_Left);
+				  return (equippedForm && equippedForm == pForm);
+			  }
+		  }
+		  return false;
+	  }
+};
+
+//0 = left hand, 1 = right. Use 0 slotmask for weapon
+EquipData EArInternal::ResolveEquippedObject(Actor * actor, UInt32 weaponSlot, UInt32 slotMask)
+{
+	EquipData foundData;
+	foundData.pForm = NULL;
+	foundData.pExtraData = NULL;
+	if(!actor)
+		return foundData;
+
+	MatchByEquipSlot matcher(actor, weaponSlot, slotMask);
+	ExtraContainerChanges* pContainerChanges = static_cast<ExtraContainerChanges*>(actor->extraData.GetByType(kExtraData_ContainerChanges));
+	if (pContainerChanges) {
+		foundData = pContainerChanges->FindEquipped(matcher, weaponSlot == MatchByEquipSlot::kSlotID_Right || slotMask != 0, weaponSlot == MatchByEquipSlot::kSlotID_Left);
+		return foundData;
+	}
+
+	return foundData;
+}
+
+EnchantmentItem* EArInternal::ResolveEquippedEnchantment(BaseExtraList * extraData)
+{
+	if (!extraData)
+		return NULL;
+	ExtraEnchantment* extraEnchant = static_cast<ExtraEnchantment*>(extraData->GetByType(kExtraData_Enchantment));
+	return extraEnchant ? extraEnchant->enchant : NULL;
+}
+
+
+
+
+	
+
+
+
+bool EArInternal::WeaponHasKeyword(TESObjectWEAP* weap, BGSKeyword* keyToCheck)
+{
+	BGSKeywordForm* keys = &weap->keyword;
+	for(UInt32 i = 0; i < keys->numKeywords; i++)
+	{
+		if (keys->keywords[i] == keyToCheck)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+EffectSetting* EArInternal::LookupCostliestEnchantmentMGEF(EnchantmentItem* pEnch)
+{
+	MagicItem::EffectItem* pEI = CALL_MEMBER_FN(pEnch, GetCostliestEffectItem)(5, false);
+	if (!pEI)
+	{
+		_MESSAGE("ERROR: Unable to retrieve costliest effect info.");
+		return NULL;
+	}
+
+	return pEI->mgef;
+}
+
+UInt32 EArInternal::LookupCostliestEnchantmentMGEFDuration(EnchantmentItem* pEnch)
+{
+	MagicItem::EffectItem* pEI = CALL_MEMBER_FN(pEnch, GetCostliestEffectItem)(5, false);
+	return (pEI) ? pEI->duration : 0;
+}
+
+void EArInternal::SetEnchantEffects(TESObjectWEAP* pWeap, EnchantmentItem* pEnch, EnchantedWeaponEquipInfo &weaponInfo, bool bApply)
+{
+	weaponInfo.weapon = NULL;
+	weaponInfo.enchantment = NULL;
+	weaponInfo.mgef = NULL;
+	weaponInfo.weaponType = 0;
+	weaponInfo.mgefIndex = 0;
+	weaponInfo.custom = false;
+
+	EffectSetting* thisMGEF = LookupCostliestEnchantmentMGEF(pEnch);
+	UInt32 mgefIdx = MGEFInfoLibrary.LookupMGEF(thisMGEF);
+	if (mgefIdx > 13) // || weapType == 0 || weapType == 8) //these weapTypes shouldn't really be possible if mgefIdx is valid
+	{
+		mgefIdx = customMGEFInfoLibrary.LookupMGEF(thisMGEF);
+		if (mgefIdx > 13)
+			return;
+		weaponInfo.custom = true;
+	}
+
+	UInt32 weapType = (pWeap) ? pWeap->type() : 1;
+	//adjust weapType into the indexical value needed for library:
+	if (weapType < 6 || weapType == 9 || (weapType == 6 && WeaponHasKeyword(pWeap, MGEFInfoLibrary.battleaxeKeyword)))
+		weapType -= 1;
+		//this will result in: 0=sword, 1=dagger, 2=waraxe, 3=mace, 4=greatsword, 5=battleaxe, 6=warhammer, 7=bow, 8=crossbow
+
+	if (bApply)
+	{
+		if (weaponInfo.custom)
+			customMGEFInfoLibrary.ApplyEffects(mgefIdx, weapType);
+		else
+			MGEFInfoLibrary.ApplyEffects(mgefIdx, weapType);
+	}
+	
+	weaponInfo.weapon = pWeap;
+	weaponInfo.weaponType = weapType;
+	weaponInfo.enchantment = pEnch;
+	weaponInfo.mgef = thisMGEF;
+	weaponInfo.mgefIndex = mgefIdx;
+
+	return;// weaponInfo;
+}
+
+
+
+void EArInternal::UpdateCurrentEquipInfo(Actor* actor)
+{
+	enchantedWeapon_map[actor].rightHand.weapon = NULL;
+	enchantedWeapon_map[actor].rightHand.enchantment = NULL;
+	enchantedWeapon_map[actor].rightHand.mgef = NULL;
+	enchantedWeapon_map[actor].rightHand.weaponType = 0;
+	enchantedWeapon_map[actor].rightHand.mgefIndex = 0;
+	enchantedWeapon_map[actor].rightHand.custom = false;
+	enchantedWeapon_map[actor].leftHand.weapon = NULL;
+	enchantedWeapon_map[actor].leftHand.enchantment = NULL;
+	enchantedWeapon_map[actor].leftHand.mgef = NULL;
+	enchantedWeapon_map[actor].leftHand.weaponType = 0;
+	enchantedWeapon_map[actor].leftHand.mgefIndex = 0; //scrub any old data
+	enchantedWeapon_map[actor].leftHand.custom = false;
+
+	//Right hand
+	EquipData rightHandEquipped = ResolveEquippedObject(actor, 1, 0);
+	if (rightHandEquipped.pForm)
+	{
+		TESObjectWEAP* rightHandWeapon = DYNAMIC_CAST(rightHandEquipped.pForm, TESForm, TESObjectWEAP);
+		if (rightHandWeapon)
+		{
+			EnchantmentItem* rightHandEnchant = rightHandWeapon->enchantable.enchantment; //Pre-enchanted?
+			if (!rightHandEnchant)
+				rightHandEnchant = ResolveEquippedEnchantment(rightHandEquipped.pExtraData); //Player-enchanted?
+
+			if (rightHandEnchant)
+				EArInternal::SetEnchantEffects(rightHandWeapon, rightHandEnchant, enchantedWeapon_map[actor].rightHand);
+		}
+
+		//If two-handed weapon, skip left hand check
+		if (enchantedWeapon_map[actor].rightHand.weaponType > 3) 
+			return;
+	}
+
+	if (enchantedWeapon_map[actor].rightHand.weaponType < 4) //If two-handed weapon, skip left hand check
+	{
+		//Left hand
+		EquipData leftHandEquipped = ResolveEquippedObject(actor, 0, 0);
+		if (leftHandEquipped.pForm)
+		{
+			TESObjectWEAP* leftHandWeapon = DYNAMIC_CAST(leftHandEquipped.pForm, TESForm, TESObjectWEAP);
+			if (leftHandWeapon)
+			{
+				EnchantmentItem* leftHandEnchant = leftHandWeapon->enchantable.enchantment; //Pre-enchanted?
+				if (!leftHandEnchant)
+					leftHandEnchant = ResolveEquippedEnchantment(leftHandEquipped.pExtraData); //Player-enchanted?
+
+				if (leftHandEnchant)
+					EArInternal::SetEnchantEffects(leftHandWeapon, leftHandEnchant, enchantedWeapon_map[actor].leftHand);
+			}
+		}
+	}
+
+}
---
+++ EA_Internal.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "skse/GameExtraData.h"
+#include "skse/GameBSExtraData.h"
+#include "skse/GameRTTI.h"
+#include <map>
+
+
+
+struct EnchantedWeaponEquipInfo
+{
+	TESObjectWEAP*		weapon;
+	UInt32				weaponType;		//0=sword, 1=dagger, 2=waraxe, 3=mace, 4=greatsword, 5=battleaxe, 6=warhammer, 7=bow, 8=crossbow
+	EnchantmentItem*	enchantment;
+	EffectSetting*		mgef;			//costliest MGEF in the Enchantment
+	UInt32				mgefIndex;		//index of MGEF in MGEFLibrary
+	bool				custom;
+};
+
+
+struct EnchantedWeaponInfo
+{
+	EnchantedWeaponEquipInfo	rightHand;
+	EnchantedWeaponEquipInfo	leftHand;
+};
+
+typedef std::map<Actor*, EnchantmentItem*>		EquippedEnchantMap;
+typedef std::map<Actor*, EnchantedWeaponInfo>	ActorEnchantedWeaponInfo;
+
+extern	EquippedEnchantMap			eq_map;
+extern	ActorEnchantedWeaponInfo	enchantedWeapon_map;
+
+//___________________________________________________________________________________________________________
+//======================= EQUIPMENT INFO METHODS TAKEN FROM PAPYRUSWORNOBJECT.CPP ===========================
+//
+//                                       Thank you, SKSE team!
+
+
+
+
+
+
+namespace EArInternal
+{
+	EquipData			ResolveEquippedObject(Actor * actor, UInt32 weaponSlot, UInt32 slotMask); //0 = left hand, 1 = right. Use 0 slotmask for weapon
+	EnchantmentItem*	ResolveEquippedEnchantment(BaseExtraList * extraData);
+	bool				WeaponHasKeyword(TESObjectWEAP* weap, BGSKeyword* keyToCheck);
+	EffectSetting*		LookupCostliestEnchantmentMGEF(EnchantmentItem* pEnch);
+	UInt32				LookupCostliestEnchantmentMGEFDuration(EnchantmentItem* pEnch);
+	void				SetEnchantEffects(TESObjectWEAP* pWeap, EnchantmentItem* pEnch, EnchantedWeaponEquipInfo &weaponInfo, bool bApply = true);
+	void				UpdateCurrentEquipInfo(Actor* actor);
+}
+
+
+
